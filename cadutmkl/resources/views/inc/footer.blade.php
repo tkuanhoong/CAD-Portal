7f38ed4c1c2451fd0938180ded7049eade213bba
MrTew <88481749+MrTew@users.noreply.github.com>
2021-08-25 23:59:53 +0800

    Start CADUTMKL with Laravel

---
+++ cadutmkl/resources/views/inc/footer.blade.php
@@ -0,0 +1,51 @@
+<!--Start Footer-->
+    
+    <div class="footer-svg">
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      viewBox="0 0 1920 80" style="enable-background:new 0 0 1920 80;" xml:space="preserve">
+      <path class="st0" d="M0,27.2c589.2,129.4,1044-69,1920,31v-60H3.2L0,27.2z"/>
+    </svg>
+    </div>
+    <div class="footer-row2">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-12 col-sm-12 text-center">
+          <a class="navbar-brand img-ctr" href="#"> <img src="images/cadlogo.png" alt="cadLogo" width="100" /></a>
+          
+          <ul class="footer-link-v2 link-hover mt30">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/programs">Programs</a></li>
+            <li><a href="/organization">Organization</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+    <hr class="hline">
+    <div class="footer-row3">
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="footer-social-media-icons">
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-facebook"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-twitter"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-instagram"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-linkedin"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-youtube"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-pinterest-p"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-vimeo-v"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-dribbble"></i></a>
+              <a href="javascript:void(0)" target="blank"><i class="fab fa-behance"></i></a>
+            </div>
+            <div class="footer-">
+              <p>Copyright &copy; 2021 CADUTMKL. All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+<!--End Footer-->
